ab75326ce34df251fba6065b34c57fcca8276cfb
caiqingchong <2273034940@qq.com>
2021-07-16 16:27:38 +0800

    第一次提交任务

---
+++ TestSDK/IKNSObject.h
@@ -0,0 +1,19 @@
+//
+//  IKNSObject.h
+//  TestSDK
+//
+//  Created by 张张凯 on 2021/7/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IKNSObject : NSObject
+
+
++ (void)TestString:(NSString *)name;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestSDK/IKNSObject.m
@@ -0,0 +1,14 @@
+//
+//  IKNSObject.m
+//  TestSDK
+//
+//  Created by 张张凯 on 2021/7/16.
+//
+
+#import "IKNSObject.h"
+
+@implementation IKNSObject
++ (void)TestString:(NSString *)name{
+    NSLog(@"~~~~~~~~~~~~your name is : %@",name);
+}
+@end
