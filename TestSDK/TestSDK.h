a873979c7a61e84916b64b5a6a1b71f3a46ea647
caiqingchong <2273034940@qq.com>
2021-07-16 16:16:34 +0800

    Initial Commit

---
+++ TestSDK/TestSDK.h
@@ -0,0 +1,18 @@
+//
+//  TestSDK.h
+//  TestSDK
+//
+//  Created by 张张凯 on 2021/7/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestSDK.
+FOUNDATION_EXPORT double TestSDKVersionNumber;
+
+//! Project version string for TestSDK.
+FOUNDATION_EXPORT const unsigned char TestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSDK/PublicHeader.h>
+
+
